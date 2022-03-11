b2965a33e170880bdb1aa5bf7fd8838642932376
ITCJ <itcj@foxmail.com>
2022-03-11 19:36:26 +0800

    风格调整

--- word_diction_extract.py
+++ word_diction_extract.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
-
 '''
 1. 总词数
 2. 不同词的个数
 3. 词频与排序
-
 4. 去除语料中的分词和词性标记，生成未加工语料
 '''
 
@@ -27,13 +25,10 @@ while True:
     j = ""
     newLine = ""
     for i in lineCache:
-        # print(i)
         if '\u4e00' <= i <= '\u9fa5': 
-            # print(i)
             j += i
             newLine += i
         if i == '/' and j != "":
-            # print(j)
             inputDict(j ,dict)
             j = "" # refresh
 
@@ -41,8 +36,6 @@ while True:
     resultStr.write(newLine)
     if not lineCache:
         break
-
-# print(dict)
 
 sum = 0
 for i in dict:
