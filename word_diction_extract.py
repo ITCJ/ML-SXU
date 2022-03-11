37550f8e82debb1b1dcb28b148fb1ff124cb776c
ITCJ <itcj@foxmail.com>
2022-03-11 17:51:20 +0800

    排序、去除标记

--- word_diction_extract.py
+++ word_diction_extract.py
@@ -16,6 +16,8 @@ def inputDict(inString, dict):
         dict[inString] = 1
 
 text = open('199801.txt', mode='r',encoding="gbk")
+resultDic = open('dicResult.txt', mode='w')
+resultStr = open('dicStr.txt', mode='w')
 dict = {}
 
 while True:
@@ -23,18 +25,38 @@ while True:
     lineCache.encode("utf-8")
 
     j = ""
+    newLine = ""
     for i in lineCache:
         # print(i)
         if '\u4e00' <= i <= '\u9fa5': 
             # print(i)
             j += i
+            newLine += i
         if i == '/' and j != "":
             # print(j)
             inputDict(j ,dict)
             j = "" # refresh
 
+    newLine += '\n'
+    resultStr.write(newLine)
     if not lineCache:
         break
 
 # print(dict)
-print("总词数（键数）：", len(dict))
+
+sum = 0
+for i in dict:
+    sum += dict[i]
+
+print("总键数：", len(dict))
+print("总词数：", sum)
+
+#排序
+dict = sorted(dict.items(), key=lambda x:x[1], reverse=False)
+
+for i in dict:
+    print(i, file=resultDic)
+
+text.close()
+resultDic.close()
+resultStr.close()
