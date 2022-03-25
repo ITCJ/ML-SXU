779e9a80cf396f1a5cbecdd2e4cfd4ee22090ea0
ITCJ <itcj@foxmail.com>
2022-03-25 15:55:38 +0800

    按行分，Train 和 Validation 文件大小大概1:4

---
+++ hw_03/divide_material.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+'''
+# target
+划分训练集4
+测试集1
+'''
+
+text = open("/home/tcj_wsl/ML-SXU/hw_03/dicStr.txt", mode='r')
+resultTrain = open("/home/tcj_wsl/ML-SXU/hw_03/retTrain.txt", mode='w')
+resultValidation = open("/home/tcj_wsl/ML-SXU/hw_03/retValidation.txt", mode='w')
+
+lineNum = 0
+while True:    
+    lineCache = text.readline()
+    
+    if not lineCache:
+        break
+
+    if (lineNum % 5) == 0:
+        resultValidation.write( lineCache )
+    else: 
+        resultTrain.write( lineCache )
+
+    lineNum += 1
